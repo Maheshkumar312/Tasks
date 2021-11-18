a8f80b50e5f3a3fa871b7530d8e0adbfdaa8002a
MaheshKumar312 <maheshkumar312d@gmail.com>
2021-11-18 08:40:45 +0530

    new files

--- Angular/angularCrudFrontend/src/app/employee.service.ts
+++ Angular/angularCrudFrontend/src/app/employee.service.ts
@@ -12,24 +12,23 @@ export class EmployeeService {
   private baseURL = environment.apiURL+"/api/employees";
 
   constructor(private httpClient: HttpClient) { }
-  
+    //get employee
   getAllEmployees(): Observable<Employee[]>{
     return this.httpClient.get<Employee[]>(this.baseURL);
   }
-  
-
+//save employee
   createEmployee(employee: Employee): Observable<Object>{
     return this.httpClient.post(this.baseURL+"/addEmployee", employee);
   }
-
+//get employee by id
   getEmployeeById(id: number): Observable<Employee>{
     return this.httpClient.get<Employee>(this.baseURL+`/${id}`);
   }
-
+//update employee
   updateEmployee(id: number, employee: Employee): Observable<Object>{
     return this.httpClient.put(this.baseURL+`/${id}`, employee);
   }
-
+//delete employee
   deleteEmployee(id: number): Observable<Object>{
     return this.httpClient.delete(this.baseURL+`/${id}`,{responseType: 'text'});
   }
